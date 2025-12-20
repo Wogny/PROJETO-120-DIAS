d04ab3f85bc7a7cc78f2d97604f06e4b183f6178
wognynote <wognyyy@hotmail.com>
2025-12-19 21:02:42 -0300

    Revert "Revert "mudanças visuais""
    This reverts commit 1ab5355d3ef7369922a3219eeb8d6a3496d9dc6e.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -32,25 +32,15 @@ const Navigation = (props) => {
         <div className="navigation-container">
           <a href="#">
             <div className="navigation-logo">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 24 24"
-                className="navigation-logo-icon"
-              >
-                <path
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M17.596 12.768a2 2 0 1 0 2.829-2.829l-1.768-1.767a2 2 0 0 0 2.828-2.829l-2.828-2.828a2 2 0 0 0-2.829 2.828l-1.767-1.768a2 2 0 1 0-2.829 2.829zM2.5 21.5l1.4-1.4M20.1 3.9l1.4-1.4M5.343 21.485a2 2 0 1 0 2.829-2.828l1.767 1.768a2 2 0 1 0 2.829-2.829l-6.364-6.364a2 2 0 1 0-2.829 2.829l1.768 1.767a2 2 0 0 0-2.828 2.829zM9.6 14.4l4.8-4.8"
-                ></path>
-              </svg>
+              <img
+                src={process.env.PUBLIC_URL + '/img/logo-mc.png'}
+                alt="Mateus Casarini"
+                className="navigation-logo-img"
+              />
               <span className="navigation-brand">Mateus Casarini</span>
             </div>
           </a>
+
           <div className="navigation-links">
             <a href="#sobre">
               <div className="navigation-link">
@@ -110,25 +100,15 @@ const Navigation = (props) => {
           <div className="navigation-mobile-header">
             <a href="#">
               <div className="navigation-logo">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                  className="navigation-logo-icon"
-                >
-                  <path
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M17.596 12.768a2 2 0 1 0 2.829-2.829l-1.768-1.767a2 2 0 0 0 2.828-2.829l-2.828-2.828a2 2 0 0 0-2.829 2.828l-1.767-1.768a2 2 0 1 0-2.829 2.829zM2.5 21.5l1.4-1.4M20.1 3.9l1.4-1.4M5.343 21.485a2 2 0 1 0 2.829-2.828l1.767 1.768a2 2 0 1 0 2.829-2.829l-6.364-6.364a2 2 0 1 0-2.829 2.829l1.768 1.767a2 2 0 0 0-2.828 2.829zM9.6 14.4l4.8-4.8"
-                  ></path>
-                </svg>
+                <img
+                  src={process.env.PUBLIC_URL + '/img/logo-mc.png'}
+                  alt="Mateus Casarini"
+                  className="navigation-logo-img"
+                />
                 <span className="navigation-brand">Mateus Casarini</span>
               </div>
             </a>
+
             <button
               id="navClose"
               aria-label="Fechar menu"
